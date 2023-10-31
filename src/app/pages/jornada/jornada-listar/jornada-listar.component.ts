39f818c431a34f09a69b15b4a550f37331200c5f
Anderson <anderson.gross98@gmail.com>
2023-10-31 12:32:56 -0300

    criação cadastro de funcionario e listar jornada

---
+++ src/app/pages/jornada/jornada-listar/jornada-listar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-jornada-listar',
+  templateUrl: './jornada-listar.component.html',
+  styleUrls: ['./jornada-listar.component.css']
+})
+export class JornadaListarComponent {
+
+}
